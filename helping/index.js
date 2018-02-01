4c61506768453271108b7019970cdfa095ba3fa2
aakash_raj <aakash.raj5@gmail.com>
2018-02-01 22:45:08 +0530

    Server returning a view

--- helping/index.js
+++ helping/index.js
@@ -6,14 +6,28 @@ const Handlebars = require('handlebars');
 const server = new Hapi.Server();
 // server.connection({ port: Number(process.argv[2] || 8080), host: 'localhost' });
 server.connection({ port: 8003, host: 'localhost' });
-
+server.register(Vision, (err) => {
+  if (err) throw err;
+});
 server.route({
   path: '/',
   method: 'GET',
   handler: (request, reply) => {
     // console.log(request.query);
-    reply('success!');
+    reply.view('index');
   },
 });
+server.views({
+  engines: {
+    html: Handlebars,
+  },
+  path: path.join(__dirname, 'templates'),
+});
 
+// server.start((err) => {
+//   console.log(err);
+//
+//
+//   console.log('Server running at:', server.info.uri);
+// });
 module.exports = server;
--- helping/index.test.js
+++ helping/index.test.js
@@ -6,11 +6,29 @@ describe('Test hapi server for a return file', () => {
     url: '/',
   };
 
-
+  // beforeAll((done) => {
+  //   Server.on('start', () => {
+  //     done();
+  //   });
+  // });
+  //
+  // afterAll((done) => {
+  //   Server.on('stop', () => {
+  //     done();
+  //   });
+  //   Server.stop();
+  // });
   test('testing if response status code is 200', (done) => {
     Server.inject(options, (response) => {
       console.log('got a response!!');
       expect(response.statusCode).toBe(200);
+      done();
+    });
+  });
+  test('testing if server returns a view', (done) => {
+    Server.inject(options, (response) => {
+      console.log('got a view!');
+      expect(response.result.toString()).toContain('<html>');
       done();
     });
   });
