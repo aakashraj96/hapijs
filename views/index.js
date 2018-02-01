37ccbd8ac470d2612feaa9ed4f48be50b5bd37f1
aakash_raj <aakash.raj5@gmail.com>
2018-02-01 14:27:25 +0530

    server returning 200 OK for 5th exercise

--- views/index.js
+++ views/index.js
@@ -5,24 +5,14 @@ const Handlebars = require('handlebars');
 
 const server = new Hapi.Server();
 // server.connection({ port: Number(process.argv[2] || 8080), host: 'localhost' });
-server.connection({ port: Number(process.argv[2] || 8080), host: 'localhost' });
-
-server.register(Vision, (err) => {
-  if (err) throw err;
-});
+server.connection({ port: 8003, host: 'localhost' });
 server.route({
   path: '/',
   method: 'GET',
   handler: (request, reply) => {
     // console.log(request.query);
-    reply.view('index', { message: request.query.name });
+    reply('sucess');
   },
-});
-server.views({
-  engines: {
-    html: Handlebars,
-  },
-  path: path.join(__dirname, 'templates'),
 });
 
 server.start((err) => {
