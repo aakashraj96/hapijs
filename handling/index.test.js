1ad55e464b411c23727d8e871e6b689b2fd34db2
aakash_raj <aakash.raj5@gmail.com>
2018-02-01 06:11:39 +0530

    tested returned file content

--- handling/index.test.js
+++ handling/index.test.js
@@ -27,16 +27,23 @@ describe('Test hapi server for a return file', () => {
       console.log('got a response!!');
       expect(response.statusCode).toBe(200);
       // expect(response.result).toBe('Hello aakash');
-      console.log(response);
+      // console.log(response);
       done();
     });
   });
   test('testing if response result is not empty', (done) => {
     Server.inject(options, (response) => {
-      console.log('got a response!!');
       // expect(response.statusCode).toBe(200);
       expect(response.result.toString().length).not.toBe(0);
-      console.log(response);
+      // console.log(response);
+      done();
+    });
+  });
+  test('testing for returned file contents', (done) => {
+    Server.inject(options, (response) => {
+      // expect(response.statusCode).toBe(200);
+      expect(response.result.toString()).toBe('<html>\n    <head><title>Hello Handling</title></head>\n    <body>\n        Hello Handling\n    </body>\n</html>\n');
+      // console.log(response);
       done();
     });
   });
