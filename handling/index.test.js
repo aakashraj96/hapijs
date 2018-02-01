350cc0a7f8865b708ae53b9640ddbca4b942b6d2
aakash_raj <aakash.raj5@gmail.com>
2018-02-01 06:09:03 +0530

    tested for non zero response size

--- handling/index.test.js
+++ handling/index.test.js
@@ -27,6 +27,16 @@ describe('Test hapi server for a return file', () => {
       console.log('got a response!!');
       expect(response.statusCode).toBe(200);
       // expect(response.result).toBe('Hello aakash');
+      console.log(response);
+      done();
+    });
+  });
+  test('testing if response result is not empty', (done) => {
+    Server.inject(options, (response) => {
+      console.log('got a response!!');
+      // expect(response.statusCode).toBe(200);
+      expect(response.result.toString().length).not.toBe(0);
+      console.log(response);
       done();
     });
   });
