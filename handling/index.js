49c774ccbd95d7e56d036760b29c6c43029b9243
aakash_raj <aakash.raj5@gmail.com>
2018-02-01 06:02:46 +0530

    Getting 200 response status code

--- handling/index.js
+++ handling/index.js
@@ -4,10 +4,20 @@ const path = require('path');
 
 const server = new Hapi.Server();
 // server.connection({ port: Number(process.argv[2] || 8080), host: 'localhost' });
-server.connection({ port: Number(process.argv[2] || 8080), host: 'localhost' });
+server.connection({ port: 8001, host: 'localhost' });
+server.register(Inert, (err) => {
+  if (err) throw err;
+});
+server.route({
+  path: '/',
+  method: 'GET',
+  handler: {
+    file: path.join(__dirname, '/index.html'),
+  },
+});
 
-
-server.start(() => {
+server.start((err) => {
+  console.log(err);
   console.log('Server running at:', server.info.uri);
 });
 
--- handling/index.test.js
+++ handling/index.test.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const Server = require('./index.js');
 
-describe('ping controller', () => {
+describe('Test hapi server for a return file', () => {
   const options = {
     method: 'GET',
     url: '/',
@@ -22,8 +22,9 @@ describe('ping controller', () => {
     Server.stop();
   });
 
-  test('responds with success for ping', (done) => {
+  test('testing if response status code is 200', (done) => {
     Server.inject(options, (response) => {
+      console.log('got a response!!');
       expect(response.statusCode).toBe(200);
       // expect(response.result).toBe('Hello aakash');
       done();
