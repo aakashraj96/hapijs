55d249b7147048e548b4e7390a6df6407d262d7c
aakash_raj <aakash.raj5@gmail.com>
2018-02-01 22:31:56 +0530

    Server returning 200 OK

--- helping/index.js
+++ helping/index.js
@@ -7,5 +7,13 @@ const server = new Hapi.Server();
 // server.connection({ port: Number(process.argv[2] || 8080), host: 'localhost' });
 server.connection({ port: 8003, host: 'localhost' });
 
+server.route({
+  path: '/',
+  method: 'GET',
+  handler: (request, reply) => {
+    // console.log(request.query);
+    reply('success!');
+  },
+});
 
 module.exports = server;
